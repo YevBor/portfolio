acb263a639fcd42a4528411ec55e93b0adeddbed
Yevgeniy Borisovsky <yev.bor@yandex.ru>
2021-11-28 19:54:02 +0200

    igor lox

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const contactBtn = document.querySelector('.intro__content-button');
-
-
-
-
-
+const hamburgerBtn = document.querySelector('.hamburger')
+const overlay = document.querySelector('.page');
 
 
 contactBtn.addEventListener('click', e =>{
     location.href='#contact';
 })
+hamburgerBtn.addEventListener('click', e =>{
+    hamburgerBtn.classList.toggle("is-active")
+    hamburgerBtn.nextElementSibling.classList.toggle("menu__content_active")
+    overlay.classList.toggle('overlay');
+})
+
+
