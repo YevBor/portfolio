2a0b19e180ac743cf1b839aeba050875e203927b
Yevgeniy Borisovsky <yev.bor@yandex.ru>
2021-12-14 19:56:11 +0200

    create menu layout

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const contactBtn = document.querySelector('.intro__content-button');
 const hamburgerBtn = document.querySelector('.hamburger')
-const overlay = document.querySelector('.page');
+const overlay = document.querySelector('.menu');
+const backContent = document.querySelector('.header__hamburger-menu');
 
 
 contactBtn.addEventListener('click', e =>{
@@ -10,6 +11,18 @@ hamburgerBtn.addEventListener('click', e =>{
     hamburgerBtn.classList.toggle("is-active")
     hamburgerBtn.nextElementSibling.classList.toggle("menu__content_active")
     overlay.classList.toggle('overlay');
+    backContent.style.height = "30%"
 })
+
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
 
 
